09a2dca143cd2af7175ae1e9637ecfd5753b3626
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-10 12:04:15 -0400

    solve problem 5

--- objectDrills.js
+++ objectDrills.js
@@ -1,14 +1,13 @@
 'use strict';
 
-
 // 1) Object initializers and methods
 const loaf = {
   flour: 300,
   water: 210,
   hydration: function() {
-    return (loaf.water / loaf.flour * 100);
+    return (loaf.water / loaf.flour) * 100;
   }
-}
+};
 
 console.log(loaf.hydration());
 
@@ -20,7 +19,7 @@ const baz = {
   fum: 'fum',
   quux: 'quux',
   spam: 'spam'
-}
+};
 
 // why does it only work without a parameter?
 
@@ -35,53 +34,86 @@ iteration(baz);
 
 // 3) Arrays in objects
 const hobbitDiet = {
-  meals: ['breakfast', 
-          'second breakfast', 
-          'elevenses', 
-          'lunch', 
-          'afternoon tea', 
-          'dinner', 
-          'supper']
-}
+  meals: [
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+  ]
+};
 
 console.log(hobbitDiet.meals[3]);
-
 
 // 4) Arrays of objects
 
 let ironman = {
   name: 'Stark',
   jobTitle: 'Engineer'
-}
+};
 
 let thor = {
   name: 'Odinson',
   jobTitle: 'Guardian of the Galaxy'
-}
+};
 
 let captainAmerica = {
   name: 'Rogers',
   jobTitle: 'Soldier'
-}
+};
 
 let avengers = [ironman, thor, captainAmerica];
 
-function teamInfo() {
-  for (let i = 0; i < avengers.length; i++) {
-    for (let key in avengers[i])
-      console.log(`${avengers[i][key]}`);
+function teamInfo(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let key in arr[i]) {
+      console.log(`${key}: ${avengers[i][key]}`);
+    }
   }
 }
 
-teamInfo();
+teamInfo(avengers);
 
 // 5) Properties that aren't there
+
+let ironman2 = {
+  name: 'Stark',
+  jobTitle: 'Engineer'
+};
+
+let thor2 = {
+  name: 'Odinson',
+  jobTitle: 'Guardian of the Galaxy',
+  boss: 'Stark'
+};
+
+let captainAmerica2 = {
+  name: 'Rogers',
+  jobTitle: 'Soldier',
+  boss: 'Stark'
+};
+
+let avengers2 = [ironman2, thor2, captainAmerica2];
+
+function teamInfo2(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]['boss']) {
+      console.log(
+        `${arr[i]['jobTitle']} ${arr[i]['name']} reports to ${arr[i]['boss']}`);
+    } else {
+      console.log(`${arr[i]['jobTitle']} ${arr[i]['name']} doesn't report to anybody.`);
+    }
+  }
+}
+
+teamInfo2(avengers2);
 
 // 6) Cracking the code
 
 // 7) Factory Functions with LOTR
 
-// 8) BONUS: A Database Search  
+// 8) BONUS: A Database Search
 
-// 8a) BONUS II: A Database Method 
-
+// 8a) BONUS II: A Database Method
