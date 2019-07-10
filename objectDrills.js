2da185fbbc98872773664f6e965a089309aa7363
plimeta <plimeta@gmail.com>
2019-07-10 15:26:29 -0500

    bonus question

--- objectDrills.js
+++ objectDrills.js
@@ -212,4 +212,20 @@ console.log(JSON.stringify(filteredCharacters2));
 
 // 8) BONUS: A Database Search
 
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query) {
+  arr.filter(function(element) {
+    }
+  }
+}
+
 // 8a) BONUS II: A Database Method
